ecdd0012ce83b907929f10da5e910bb187c035f8
naomiEve <0xlunaric@gmail.com>
2023-08-04 19:00:48 +0200

    refactor: cache the type name once at console logger creation.

--- Tako.Common/Logging/ConsoleLogger.cs
+++ Tako.Common/Logging/ConsoleLogger.cs
@@ -6,33 +6,46 @@
 /// <typeparam name="T">The class to log for.</typeparam>
 public class ConsoleLogger<T> : ILogger<T>
 {
+	/// <summary>
+	/// The type's name.
+	/// </summary>
+	private readonly string _typeName;
+
+	/// <summary>
+	/// Constructs a new console logger.
+	/// </summary>
+	public ConsoleLogger()
+	{
+		_typeName = typeof(T).Name;
+	}
+
 	/// <inheritdoc/>
 	public void Debug(string message)
 	{
-		Console.WriteLine($"[DEBUG ({DateTime.Now})] {typeof(T).Name}: {message}");
+		Console.WriteLine($"[DEBUG ({DateTime.Now})] {_typeName}: {message}");
 	}
 
 	/// <inheritdoc/>
 	public void Error(string message)
 	{
-		Console.WriteLine($"[ERROR ({DateTime.Now})] {typeof(T).Name}: {message}");
+		Console.WriteLine($"[ERROR ({DateTime.Now})] {_typeName}: {message}");
 	}
 
 	/// <inheritdoc/>
 	public void Fatal(string message)
 	{
-		Console.WriteLine($"[FATAL ({DateTime.Now})] {typeof(T).Name}: {message}");
+		Console.WriteLine($"[FATAL ({DateTime.Now})] {_typeName}: {message}");
 	}
 
 	/// <inheritdoc/>
 	public void Info(string message)
 	{
-		Console.WriteLine($"[INFO ({DateTime.Now})] {typeof(T).Name}: {message}");
+		Console.WriteLine($"[INFO ({DateTime.Now})] {_typeName}: {message}");
 	}
 
 	/// <inheritdoc/>
 	public void Warn(string message)
 	{
-		Console.WriteLine($"[WARN ({DateTime.Now})] {typeof(T).Name}: {message}");
+		Console.WriteLine($"[WARN ({DateTime.Now})] {_typeName}: {message}");
 	}
 }
